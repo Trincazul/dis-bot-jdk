dc2386ae9229b9f8fe37eeb95c5af1af9718bb99
Endriw Villa <endriwvilla@MacBook-Air-de-Endriw.local>
2020-05-02 17:26:37 -0300

    Adicionando a estrutura principal do bot

---
+++ src/main/java/com/github/trincazul/Main.java
@@ -0,0 +1,31 @@
+package com.github.trincazul;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Só colocar o token aqui !
+        String token = "token";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // escuta do bot
+        api.addMessageCreateListener(event -> {
+            if (event.getMessage().getContent().equalsIgnoreCase("!status")) {
+                event.getChannel().sendMessage("Okay");
+            }
+            if (event.getMessage().getContent().equalsIgnoreCase("!teste")) {
+                event.getChannel().sendMessage("teste de bot ok");
+            }
+            if (event.getMessage().getContent().equalsIgnoreCase("!start")) {
+                event.getChannel().sendMessage("tudo compelto !");
+            }
+
+        });
+
+        // Print para invitar a url do bot
+        System.out.println("Você pode invitar o Bot usando a URL: " + api.createBotInvite());
+    }
+
+}
---
+++ src/main/java/com/github/trincazul/MyListener.java
@@ -0,0 +1,5 @@
+package com.github.trincazul;
+
+public class MyListener implements MessageCreateListener {
+
+    }
